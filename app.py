a8378dcef8bab1e3a7da594b7a31ee9f383a37c2
james-csy <jameschong@berkeley.edu>
2023-07-12 15:59:53 -0400

    make new route

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import csv
 app = Flask(__name__)
 
 
-@app.route('/')
-def importSanctionList():
-    def readSanctionList():
+#extracts names from sanction lists and outputs list of names
+def readSanctionList():
         with open('files/sdn.csv', mode='r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             names = []
@@ -14,9 +13,16 @@ def importSanctionList():
                 names.append(row["NAME"])
             #print(f'Processed {line_count} lines.')
         return names
-    sanctionedNames = readSanctionList()
 
-    return sanctionedNames
+@app.route('/')
+def importSanctionList():
+    return readSanctionList()
+
+@app.route('/search')
+def SearchSanctionList():
+    inSanctionList = False
+    return str(inSanctionList)
+
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=5000, debug=True)
