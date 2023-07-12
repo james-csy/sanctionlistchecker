b71f1b26753a902b8cc7cf64fdb2e2d3295b62bc
james-csy <jameschong@berkeley.edu>
2023-07-12 11:08:23 -0400

    able to read csv sanction list file

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+
+
+    def readSanctionList():
+        with open('files/sdn.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                print(row)
+                line_count += 1
+                if line_count == 20:
+                    break
+            print(f'Processed {line_count} lines.')
+    readSanctionList()
+    
     return '<h1>Check your files for mentions of People/Companies on the US Sanctions List</h1>'
