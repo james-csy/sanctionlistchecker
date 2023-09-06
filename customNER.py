240bced2cde3d0d6d0e5c4fb4edc99be5f8f533f
james-csy <jameschong@berkeley.edu>
2023-09-05 17:37:31 -0700

    switching to main

--- customNER.py
+++ customNER.py
@@ -1,10 +1,23 @@
 import spacy
 import json
 
+from spacy.tokens import DocBin
 
 
 def customModel():
     nlp = spacy.load("en_core_web_sm")
     with open("NER_data.json", "r") as read_file:
         data = json.load(read_file)
-        trainingData = data["annotations"]
+        training_data = data["annotations"]
+
+    # the DocBin will store the example documents
+    db = DocBin()
+    for text, annotations in training_data:
+        doc = nlp(text)
+        ents = []
+        for start, end, label in annotations["entities"]:
+            span = doc.char_span(start, end, label=label)
+            ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+    db.to_disk("./train.spacy")
